442ea6217258013194a44a07c16f1d352cc5169d
juhijha0406 <81295328+juhijha0406@users.noreply.github.com>
2021-05-12 02:34:29 +0530

    Update Countdownlist.js

--- src/component/Countdownlist.js
+++ src/component/Countdownlist.js
@@ -11,13 +11,13 @@ class Countdownlist extends Component {
          seconds : 0
     }
  console.log('this.probs' , this.props);
-//  this will also print  this probs  with value
+
 }
 
 componentWillMount()
 {
     this.getTimeUntil(this.props.deadline);
-    // console.log(this.probs.deadline);
+   
 }
 
 componentDidMount(){
@@ -43,12 +43,12 @@ getTimeUntil (deadline){
 
      console.log(seconds,minutes,hours,days);
      this.setState({days : days , hours:hours ,minutes:minutes, seconds,seconds });
-    // this will only take value which we have to do console
+   
 }
 
 
 render () {
-    // this.getTimeUntil(this.props.deadline);
+
     return(
     <div>
     <div  className='style'>
@@ -56,7 +56,7 @@ render () {
        <div className='dis1 style1'>{this.leading0(this.state.hours)}hours</div>
        <div className='dis1 style1'>{this.leading0(this.state.minutes)}minutes</div>
        <div className='dis1 style1'>{this.leading0(this.state.seconds)}seconds</div>
-       {/* here we are passign state   */}
+       
     </div>
      </div>
     )
